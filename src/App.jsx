c7334407d4847fc27a415d47f10483d8c13be107
radudeacu <deacuradu@gmail.com>
2024-05-07 12:34:05 +0300

    info section done

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,10 @@ const App = () => {
       <Navbar/>
 
       <div className="px-0 md:px-5 2xl:px-20">
-        <Info/>
+        <Info 
+          title="Dashboard" 
+          subTitle="Monitor your financial activities"
+        />
         <Stats/>
 
         <div className="w-full flex flex-col-reverse md:flex-row items-center gap-10">
