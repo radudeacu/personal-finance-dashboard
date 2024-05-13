6bbbdf1d052bd95a18f6cac28714586c4fe0fad2
radudeacu <deacuradu@gmail.com>
2024-05-13 13:54:02 +0300

    stats section done

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BsCashCoin, BsCurrencyDollar } from 'react-icons/bs';
+import { IoMdArrowDown, IoMdArrowUp } from 'react-icons/io';
 import { SiCashapp } from 'react-icons/si';
 
 const data = [
@@ -31,8 +32,30 @@ const ICON_STYLES = [
 
 const Stats = () => {
   return (
-    <div>Stats</div>
+    <div className='flex flex-col md:flex-row items center jutisfy-between gap-8 2xl:gap-30 mb-20'>
+      {
+        data.map((item, index)=> (
+          <div key={index + item.label} className="w-full 2xl:min-w-96 flex item-scenter jutisfy-between gap-5 px-4 md:px-8 py-12 rounded-lg bg-gray-50 dark:bg-slate-800 border border-gray-100 dark:border-slate-900">
+            <div className='flex items-center gap-2'>
+              <div className={`w-12 h-12 flex items-center justify-center rounded-full ${ICON_STYLES[index]}`}>{item.icon}
+              </div>
+              <div className="space-y-3">
+                <span className="text-gray-600 dark:text-gray-400 text-base md:text-lg">{item.label}</span>
+                <p className="text-xl 2xl:text-3xl font-medium text-black dark:text-gray-400">${item.amount}</p>
+              </div>
+            </div>
+            <div>
+              <p className={`flex gap-1 items-center text-base md:text-lg font-semibold ${item.increase > 0 ? "text-emerald-500": "text-red-500"}`}>
+                {item.increase > 0 ? <IoMdArrowUp /> : <IoMdArrowDown />} 
+                {Math.abs(item.increase)} %
+                </p>
+              <span className='text-xs md:text-sm text-gray-600 dark:text-gray-500'>Compare to last year</span>
+            </div>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
-export default Stats
+export default Stats;
