340a9fe4f13f5651442af04fc99d3a34f1cb25c5
radudeacu <deacuradu@gmail.com>
2024-05-13 14:13:28 +0300

    chart done + created reusable title component

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React from 'react';
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import Title from './title';
+
+const data = [
+  { label: "January", income: 5000, expense: 3000 },
+  { label: "February", income: 4900, expense: 3200 },
+  { label: "March", income: 5500, expense: 3500 },
+  { label: "April", income: 4800, expense: 2900 },
+  { label: "May", income: 5100, expense: 3800 },
+  { label: "June", income: 5800, expense: 3300 },
+  { label: "July", income: 5400, expense: 3100 },
+  { label: "August", income: 5300, expense: 3600 },
+  { label: "September", income: 5700, expense: 3700 },
+  { label: "October", income: 5900, expense: 3900 },
+  { label: "November", income: 6000, expense: 3700 },
+  { label: "December", income: 6600, expense: 4200 },
+];
 
 const Chart = () => {
   return (
-    <div>Chart</div>
+    <div className='w-full md:w-2/3'>
+      <Title title="Transaction Activity"/>
+
+      <ResponsiveContainer width={"100%"} height={500} className='mt-5'>
+        <LineChart width={500} height={300} data={data}>
+          <CartesianGrid strokeDasharray='3 3'/>
+          <YAxis />
+          <XAxis />
+          <Legend />
+          <Line type="monotone" dataKey={"income"} stroke='#8884d8' />
+          <Line type="monotone" dataKey={"expense"} stroke='#82ca9d'/>
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   )
 }
 
---
+++ src/components/title.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Title = ( {title} ) => {
+  return <p className="text-xl 2xL:text-3xl font-semibold text-gray-600 dark:text-gray-500">{title}</p>
+}
+
+export default Title;
